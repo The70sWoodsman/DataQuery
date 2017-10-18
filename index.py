5fb9a80770c84179a9ce10e5f00d517407e8ead3
Chris Witheridge <chris.witheridge@smartxpo.com>
2017-10-18 11:35:18 +0100

    Replace Hello World

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Hello? Is there anybody in there?',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
